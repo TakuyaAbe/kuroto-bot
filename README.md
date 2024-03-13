5c7e974a58d6068e864210cf170f30e38d8fe84b
Takuya Abe <t.abe@4digit.jp>
2024-03-13 19:14:36 +0900

    adjustments

---
+++ README.md
@@ -0,0 +1,21 @@
+# kuroto-bot
+
+Claude 3 Opusを使って俳句を作るbotです
+
+## 必要なもの
+
+- Node.js v20 or higher
+- npm v10 or higher
+- Anthropic API key (envファイルに入れる)
+- Twitter API key (envファイルに入れる)
+
+
+## 動かし方
+
+1. `npm i` (初回のみ)
+2. `npm start`
+
+
+## チューニング
+
+- `/index.ts` に記載のプロンプトを調整してください
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,51 @@
 import Anthropic from "@anthropic-ai/sdk";
 import * as dotenv from "dotenv";
-import chalk from "chalk";
 import { TwitterApi } from "twitter-api-v2";
 
 dotenv.config();
 
-console.log(process.env.ANTHROPIC_API_KEY);
-
 const anthropic = new Anthropic({
-	apiKey: process.env.ANTHROPIC_API_KEY, // defaults to process.env["ANTHROPIC_API_KEY"]
+	apiKey: process.env.ANTHROPIC_API_KEY,
 });
 
 const msg = await anthropic.messages.create({
 	model: "claude-3-opus-20240229",
 	max_tokens: 32,
 	temperature: 1,
-	system:
-		"クロード、あなたは高校時代に俳句甲子園全国大会に出場したこともある俳人です。大学でも俳句会で活動を続けていました。有季定型、季重なり(季語が１句に複数出現する)は避けてください。",
+	system: `クロード、あなたは高校時代に俳句甲子園全国大会に出場したこともある俳人です。
+		清冽な感性が評価されています。
+		大学生になった今も俳句会で活動を続けています。`,
 	messages: [
 		{
 			role: "user",
 			content: [
 				{
 					type: "text",
-					text: "春の季語で俳句を一句作ってもらえますか？季語が入っていれば「春」と言う言葉は入れなくていいです。驚きや発見がある景で作ってください。改行・スペースは不要です。前後の説明は不要です。",
+					text: `春の季語で俳句を一句作ってもらえますか？
+					「土筆」「桜」「犬」「蕗の薹」は禁止です。
+					季重なり(季語が１句に複数出現する)は避けてください。
+					季語が入っていれば「春」と言う言葉は入れなくていいです。
+					驚きや発見がある景で作ってください。
+					改行・スペースは不要です。
+					有季定型で作ってください。
+					字余り・字足らずは避けてください。
+					口語で作ってください。
+					前後の説明は不要です。`,
 				},
 			],
 		},
 	],
 });
+
 console.log(msg);
 
 // https://github.com/plhery/node-twitter-api-v2/blob/HEAD/doc/basics.md
-// Instantiate with desired auth type (here's Bearer v2 auth)
 const twitterClient = new TwitterApi({
 	appKey: process.env.TWITTER_API_KEY,
 	appSecret: process.env.TWITTER_API_KEY_SECRET,
-	// Following access tokens are not required if you are
-	// at part 1 of user-auth process (ask for a request token)
-	// or if you want a app-only client (see below)
 	accessToken: process.env.TWITTER_ACCESS_TOKEN,
 	accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-// const appOnlyClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
 
 await twitterClient.v2.tweet(
 	`${msg.content[0].text} / kuroto\n\nclaude-3-opus-20240229, input_tokens: ${msg.usage.input_tokens} / output_tokens: ${msg.usage.output_tokens}`,
